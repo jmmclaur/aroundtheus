919183bbe7820df0f2e24ef9162d4c1ea24f0287
Jessica McLaurin <mclaurinjessica3@gmail.com>
2024-01-22 22:51:48 -0600

    js script

---
+++ scripts/index.js
@@ -0,0 +1,28 @@
+const initialCards = [
+  {
+    name: "Rocky Mountain National Park",
+    link: "https://media.istockphoto.com/id/478656454/photo/maroon-bells-autumn-aspen-trees-lake-reflections-aspen-colorado.jpg?s=1024x1024&w=is&k=20&c=CrZVgb_84cwGhfGZwFFcDXZqSx9dLvCTMfy-oBXJKYU=",
+  },
+  {
+    name: "Breckenridge",
+    link: "https://media.istockphoto.com/id/1276445053/photo/breckenridge-colorado-usa-in-winter.jpg?s=1024x1024&w=is&k=20&c=-sZ8OKxjprRniFEYepOl0oBAOyP8bIohcwPNTzNzHVo=",
+  },
+  {
+    name: "Frisco",
+    link: "https://media.istockphoto.com/id/1097392008/photo/friends-hiking-in-the-muntains-on-winter-break.jpg?s=1024x1024&w=is&k=20&c=B6fiAv_Q9EJSsu8n4O_7J3p7HdTZGE0OtHFwJjEgeDY=",
+  },
+  {
+    name: "Estes Park",
+    link: "https://media.istockphoto.com/id/506187637/photo/north-american-elks.jpg?s=1024x1024&w=is&k=20&c=7Xx_HwEZu_FBzvn96jb75BrrO8i1H87Vu319_HsQQbc=",
+  },
+  {
+    name: "Maroon Bells",
+    link: "https://media.istockphoto.com/id/185850441/photo/maroon-bell-mountains.jpg?s=1024x1024&w=is&k=20&c=Kd0JaITNN87nUbBWMa6FojI6j23HnBMVQvaMKMUpXN8=",
+  },
+  {
+    name: "Longs Peak",
+    link: "https://media.istockphoto.com/id/914687774/photo/full-moon-in-longmont.jpg?s=1024x1024&w=is&k=20&c=K9XXTxZugTxlaS5w0OBtwCQhqtIIowMOZfpu1E4OkLQ=",
+  },
+];
+
+console.log(initialCards);
